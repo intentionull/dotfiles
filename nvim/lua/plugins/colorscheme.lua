c44f6edc3df0edafd2ceaca6a6104a6f334209a3
Jonathan Clark <hello@doitalldev.com>
2025-04-21 13:18:09 -0700

    fix: remove navic

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -31,7 +31,6 @@ return {
             information = { "undercurl" },
           },
         },
-        navic = { enabled = true, custom_bg = "lualine" },
         neotest = true,
         neotree = true,
         noice = true,
