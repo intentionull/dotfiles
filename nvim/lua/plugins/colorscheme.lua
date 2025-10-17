fe2edf19d8b0a67496388309146d64a2aab4ddb3
piha_io <hello@piha.io>
2025-10-17 09:54:48 -0700

    feat: back to lazyvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,63 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
-	transparent_background = true, -- disables setting the background color.
-	auto_integrations = true,
-	flavour = "latte", -- latte, frappe, macchiato, mocha
-	float = {
-		transparent = false, -- enable transparent floating windows
-		solid = true, -- use solid styling for floating windows, see |winborder|
-	},
-	-- term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-	-- dim_inactive = {
-	-- 	enabled = true, -- dims the background color of inactive window
-	-- 	shade = "dark",
-	-- 	percentage = 0.15, -- percentage of the shade to apply to the inactive window
-	-- },
+  -- add gruvbox
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      lsp_styles = {
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        fzf = true,
+        grug_far = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        mini = true,
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        snacks = true,
+        telescope = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.special.bufferline").get_theme()
+          end
+        end,
+      },
+    },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
 }
